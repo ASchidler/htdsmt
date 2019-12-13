8ef89817fa62f52f000de8d9414199e9decbdba5
Andre Schidler <andre@schidler.at>
2019-12-13 12:54:22 +0100

    Minor changes for benchmarking

--- weighted/run_weighted_benchmark.py
+++ weighted/run_weighted_benchmark.py
@@ -5,7 +5,8 @@ import frasmt_solver as solver
 
 instance = sys.argv[1]
 weighted_instances = 2
-tmp_dir = '/tmp'
+tmp_dir = sys.argv[2]
+
 fl = 'ghtw_solver2'
 
 tm_start = time.time()
@@ -13,7 +14,7 @@ res = solver.solve(tmp_dir, fl, instance, htd=False, force_lex=False,
                        sb=False, heuristic_repair=False, clique_mode=1,
                        weighted=False, timeout=900)
 
-print(f"GHTW: {res.size} in {time.time() - tm_start}")
+print(f"GHTW: {res.size} - width: {res.size} in {time.time() - tm_start}")
 
 for i in range(1, weighted_instances+1):
     instance = sys.argv[1] + f".w{i}"
