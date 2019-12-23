98c92020f2ce9c7d225b8e567fdc556827c1da92
Andre Schidler <andre@schidler.at>
2019-12-23 19:08:26 +0100

    Adapted order in approx benchmark

--- weighted/run_approximation_benchmark.py
+++ weighted/run_approximation_benchmark.py
@@ -10,21 +10,16 @@ tmp_dir = sys.argv[2]
 
 fl = 'ghtw_solver2'
 
-for i in range(1, weighted_instances+1):
+for bb, i in ((x, y) for x in [False, True] for y in range(1, weighted_instances + 1)):
     instance = sys.argv[1] + f".w{i}"
 
-
+    tm_start = time.time()
     hypergraph_in = Hypergraph.from_file(instance, fischl_format=False)
     hypergraph2 = Hypergraph.from_file(instance, fischl_format=True, weighted=True)
 
     if hypergraph_in is None or (hypergraph2 is not None and len(hypergraph2.edges()) > len(hypergraph_in.edges())):
         hypergraph_in = hypergraph2
 
-    tm_start = time.time()
-    weight1 = wub.greedy(hypergraph_in, bb=False)
-    tm1 = time.time()
-    weight2 = wub.greedy(hypergraph_in, bb=True)
-    tm2 = time.time()
+    weight1 = wub.greedy(hypergraph_in, bb=bb)
 
-    print(f"Heuristisc WGHTW{i}: {weight1} in {tm1 - tm_start}")
-    print(f"Heuristisc BBWGHTW{i}: {weight1} in {tm2 - tm1}")
+    print(f"Heuristisc WGHTW{i}: BB: {bb} {weight1} in {time.time() - tm_start}")
